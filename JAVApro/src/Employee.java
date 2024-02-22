e80a3fcfe559570f8153aab814d1c338a07e69e3
novi <novi@192.168.8.171>
2024-02-22 21:26:08 +0300

    add variable

--- JAVApro/src/Employee.java
+++ JAVApro/src/Employee.java
@@ -2,8 +2,8 @@
 public class Employee {
 public static void main(String[]args) {
 	System.out.println("hi");
-		System.out.println("nnnnnn");
-	System.out.println("hi")
-
+		int c=90;
+		int x=c;
+		System.out.println(x);
 }
 }

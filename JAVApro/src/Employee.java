9e87d48a90bfd71d905024832e69a83c57a5d8fc
novi <novi@localhost>
2024-02-24 20:21:58 +0300

    atributs has been add

--- JAVApro/src/Employee.java
+++ JAVApro/src/Employee.java
@@ -1,9 +1,6 @@
 
-public class Employee {
-public static void main(String[]args) {
-	System.out.println("hi");
-		int c=90;
-		int x=c;
-		System.out.println(x);
-}
+public abstract class Employee {
+protected int ID;
+protected int age;
+public int Elist[];
 }

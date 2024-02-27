82a6a12def1177497499d4d41495743047fa3123
NorahD20 <160909807+NorahD20@users.noreply.github.com>
2024-02-27 12:01:01 +0300

    Update Customer.java

--- JAVApro/src/Customer.java
+++ JAVApro/src/Customer.java
@@ -5,6 +5,20 @@ private int numOfCustomer;
 private char Level;
 private int [] cList;
 
-  p
+  public Customer ( int id , char Level ){
+    ID = id ;
+    Level = lev ;
+    numOfCustomer = 0 ;
+    cList = new int[300] ;
+  }////
+
+
+  public void addCustomer(Customer C){
+if( numOfCustomer < cList.length ){
+  cList [ numOfCustomer++ ] = C;
+  System.out.println("
+
+}
+  }//////
 
 }////class

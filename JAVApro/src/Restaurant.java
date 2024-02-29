cc6234838f8fdbe0883c1004bdb4ec90e02beed0
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-02-29 21:06:01 +0300

    Update Restaurant.java

--- JAVApro/src/Restaurant.java
+++ JAVApro/src/Restaurant.java
@@ -5,7 +5,7 @@ private int numOfR;//number of restaurant
 private Restaurant [] Rlist ;
 
 
-public Restaurant( int codeOfRestaurant ){
+public Restaurant( int COR ){
 codeOfRestaurant = COR ;
 numOfR = 0 ;
 Rlist = new Restaurant[10] ;

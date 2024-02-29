b5d879a49832d487a465e4913fce08f8abd2468f
NorahD20 <160909807+NorahD20@users.noreply.github.com>
2024-02-29 22:32:49 +0300

    Update Restaurant.java

--- JAVApro/src/Restaurant.java
+++ JAVApro/src/Restaurant.java
@@ -1,25 +1,39 @@
 public class Restaurant {
 private String nameOfRestaurant ;
-private int numOfR;//number of restaurant
 private String [] Rlist ;
-private Customer [] tList ; 
 
 
-public Restaurant( String name ){
+
+public Restaurant( String name  , int num){
 nameOfRestaurant =name;
-numOfR = 0 ;
-tlist = new Customer[10] ;
+numberSection = num ;
 Rlist = new String []{ "shiro sushi  " , "burgerizzer" , "Shawarmer" , "ALbaik" , "Maestro Pizza"} ; 
 }///
 
 
-public int  SearchRestaurant (String name ){
+public int SearchRestaurant (String name ){
 
   for ( int i =0 ; i < Rlist.length ; i++ )
   if ( Rlist[i].equalsIgnoreCase(name) ) 
-  return (i+1) ;
+  return i ;
   return -1 ;
 }////
+
+ public boolean removeRestaurant( String name ) {
+
+int loc = SearchRestaurant (name ) ;
+
+   if ( loc != -1 )
+   {
+     Rlist[loc] = Rlist[ Rlist.length -1 ];
+     Rlist[ Rlist.length -1 ] = null ;
+     return true ; 
+   }
+   return false; 
+
+
+
+ }////
 
   
 

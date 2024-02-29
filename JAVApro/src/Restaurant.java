656583877a34f2894d5c2022376c7931e37a5e29
NorahD20 <160909807+NorahD20@users.noreply.github.com>
2024-02-29 21:05:16 +0300

    Update Restaurant.java

--- JAVApro/src/Restaurant.java
+++ JAVApro/src/Restaurant.java
@@ -2,5 +2,20 @@
 public class Restaurant {
 private int codeOfRestaurant;
 private int numOfR;//number of restaurant
+private Restaurant [] Rlist ;
 
-}
+
+public Restaurant( int codeOfRestaurant ){
+codeOfRestaurant = COR ;
+numOfR = 0 ;
+Rlist = new Restaurant[10] ;
+}///
+
+  
+
+
+
+
+
+  
+}///class

1436e862c704889cc045f2925374f10fc4a324f9
Alaaa05 <160910172+Alaaa05@users.noreply.github.com>
2024-03-08 15:02:13 +0300

    Update Hotel.java

--- JAVApro/src/Hotel.java
+++ JAVApro/src/Hotel.java
@@ -1,7 +1,39 @@
 
-public class Hotel { 
- private int days:;
+public class Hotel extends Carnival { 
+ private int days;
  private String email;
+ private String phoneNumber;
+ private double coast;
+
+
+ public Hotel(int Days, String Email, String PN, double Coast ){
+ days=Days;
+ email= Email;
+ phoneNumber= PN;
+ coast=Coast;
+ }
+
+ public boolean isFull(){
+ if(nOfR==RList.length)
+  return true;
+  return false;
+ }
+ 
+ public boolean addGuest(Customer C) {
+ if(isFull()==false){
+ RList[nOfR++]= C;
+  return true;
+ }
+  return false;
+ } 
+ 
+
+ 
+
+ 
+
+ 
+ 
  
   
 

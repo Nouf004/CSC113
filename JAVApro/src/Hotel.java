8b3a98df164683074fdeb097078d4dd3f534cafc
Alaaa05 <160910172+Alaaa05@users.noreply.github.com>
2024-03-08 16:50:18 +0300

    Update Hotel.java

--- JAVApro/src/Hotel.java
+++ JAVApro/src/Hotel.java
@@ -1,18 +1,21 @@
 
 public class Hotel extends Carnival { 
  private int days;
- private String email;
- private String phoneNumber;
  private double coast;
  private numOfCustomer;
+ private int num;
+ private int check_inDay;
+ private int check_outDay;
 
 
- public Hotel(int Days, String Email, String PN, double Coast ){
+ public Hotel(int Days, String Email, String PN, double Coast, int checkIn, int checkOut ){
  days=Days;
  email= Email;
  phoneNumber= PN;
  coast=Coast;
-  numOfCustomer=0;
+ numOfCustomer=0;
+ check_inDay=checkIn;
+ check_outDay=checkOut;
  }
 
  public boolean isFull(){
@@ -21,13 +24,36 @@ public class Hotel extends Carnival {
   return false;
  }
  
- public boolean addCustomer(Customer c) {
- if(isFull()==false){
- CList[numOfCustomer++]= C;
+ public boolean addCustomer(Customer c , int num ) {
+ if(searchRoom(num)!=-1){
+ CList[num-1]= C;
+  numOfCustomer++;
   return true;
  }
   return false;
  } 
+
+ public int searchRoom(int num){
+  if( Clist[num-1]==null)
+   return num;
+   retrun -1;
+ }
+
+ public void removeCustomer(int Rnum){
+  int count=0;
+  int duration= check_outDay-check_inDay;
+  for(int i=0; i<duration; i++)
+   count++;
+  if(count>duration) 
+  Clist[Rnum-1]=null;
+  numOfCustomer--;
+ }
+   
+   
+
+  
+  
+   
 
  
  

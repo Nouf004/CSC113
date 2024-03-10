2dd5379aee476e2f61961d71578abbdd5aaaf18d
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-03-11 00:39:47 +0300

    Update TestC.java

--- JAVApro/src/TestC.java
+++ JAVApro/src/TestC.java
@@ -1,26 +1,30 @@
 import java.util.*;
-
 public class TestC {
 public static void main(String []args) {
 Scanner in=new Scanner(System.in);
-System.out.println("Welcome to CS world)");
 int num;
 themePark City=new themePark();
 Restaurant R1=new Restaurant();
 Hotel CsHotel=new Hotel();
+Customer []cList=new Customer[300];//check about it
+int counterOfCustemer=0;
+
 do {
+   System.out.println("********* Welcome to CS world *********");
 	System.out.println("Choose the following options:");
 	System.out.println("==============================");
 	System.out.println("1-Do you want to enter as an employee?");
-	System.out.println("2-Do you want to enter as an Customer");
+	System.out.println("2-Do you want to enter as an Customer?");
+   System.out.println("3-Enter -1 to log out from the application");
+   
 	 num=in.nextInt();
-	 
+
 	switch(num) {
 	case 1:
 	System.out.println("Enter your ID:");
 	String IDemployee=in.next();
 	System.out.println("Enter your Age:");
-		double ageEmployee=in.nextDouble();
+		int ageEmployee=in.nextInt();
 		
 		System.out.println("Do you want to participate as Daily Employee  enter 1 or Hourly Employee enter 2");
 		int choose=in.nextInt();
@@ -48,19 +52,129 @@ do {
 		}//Switch case for hour and day employee work
 		
 		break;
-		
-		
 	case 2:
+   
 		System.out.println("Enter your ID pleas:");
 		int id=in.nextInt();
 			System.out.println("Enter your level weather it's Gold as G or Silver as S or Bronze as B");
 			char level=in.next().charAt(0);
-			Customer cus1=new Customer(id,level);
-		
-			City.addCustomer(cus1);
+         
+         //Customer[] customerlist=new Customer[300];
+         //for ( int i = 0 ; i < customerlist.length ; i++){
+         //customerlist[i] = new Customer(id,level);
+         //City.ArrayOfCustomer(customerlist[i]);}
+         //Customer cus1;
+         
+         cList[counterOfCustemer++]=new Customer(id,level);
+         
+		 //cus1=new Customer(id,level);
+			//City.addCustomer(cus1);
+         
 			String answer;
-			int number;
-			do {
+      
+         int number;
+         System.out.println("1-Check Prize?");
+				
+				System.out.println("2-Search for City?");
+				
+				System.out.println("3-Search for Restaurant?");
+				
+				System.out.println("4-Do you have any complaints about any Restaurant?");
+				
+				System.out.println("5-Do you want to have a book a room ?");
+				
+				System.out.println("6-Do you want to Display your info?");
+				
+				System.out.println("enter 0 if you don't have anything to do.");
+				 number=in.nextInt();
+             
+         while(number!=0){
+            switch(number) {
+	
+				case 1:
+					System.out.println("Please enter your score:");
+					int numOfScore=in.nextInt();
+					
+					City.prize(numOfScore);///check if there is any error
+					break;
+					
+				case 2:
+           String nameOfCity;
+			System.out.println("Enter the name of city you want to check:");
+         in.nextLine();
+			 nameOfCity=in.nextLine();
+         
+			int place=City.SearchNC(nameOfCity);
+         
+         if(place!=-1)
+         
+         System.out.println("The place is at section:"+(place+1));
+         
+         else 
+         System.out.println("There is no city called like this.");
+			break;
+			
+				case 3:
+					System.out.println("Enter the name of the Restaurant:");
+					String nameOfRestaurant=in.next();
+					//R1.SearchRestaurant(nameOfRestaurant);
+               int loc=R1.SearchRestaurant(nameOfRestaurant);
+               if(loc!=-1)
+               System.out.println("The Restaurant is in gate :"+(loc+1));
+               else System.out.println("There is no Restaurant called like this.");
+
+					break;
+					
+				case 4:
+					System.out.println("Enter the name of the Restaurant that you recommend to remove:");
+               
+					System.out.println("Shiro,Burgerizzer,Shawarmer,ALbaik ,Maestro");
+             
+					String Rname=in.next();
+               
+					//Rname =remove name of the Restaurant.
+               
+					if(R1.removeRestaurant(Rname))
+               System.out.println("We have listend to your request.");
+               else 
+               
+               System.out.println("You might misspelled the name, check again");
+					break;
+					
+				case 5:
+				if(CsHotel.isFull())
+					System.out.println("Sorry you can't book in this CS Hotel");
+				else {
+					
+				System.out.println("***Please enter the dates***");
+            System.out.println(" Check in:");
+           	
+				int date_Chekin=in.nextInt();
+            System.out.println("Check out:");
+				int date_Checkout=in.nextInt();
+			
+				
+				System.out.println("Enter number of the room that you want to check in:");
+				int numOfRoom=in.nextInt();
+				
+				if(CsHotel.AddRoom(cList[counterOfCustemer], numOfRoom)==true) {
+					System.out.println("Congrats you have booked a room");
+					System.out.println("the total coast="+CsHotel.calculatePrice(date_Checkout, date_Chekin));
+				}
+				else 
+					System.out.println("We are sorry there is no room available");
+				
+				}//else
+            
+				break;
+            
+            case 6://work on it
+            System.out.println("*****Your Info*****");
+				System.out.println("==================");
+				System.out.println(cList[counterOfCustemer].toString());
+            break;
+			}//switch cus number
+				
 				System.out.println("1-Check Prize?");
 				
 				System.out.println("2-Search for City?");
@@ -73,82 +187,16 @@ do {
 				
 				System.out.println("6-Do you want to Display your info?");
 				
-				System.out.println("enter x if you don't have anything to do.");
+				System.out.println("enter 0 if you don't have anything to do.");
 				 number=in.nextInt();
+             
+			}//while(number=='x');
 			
-				switch(number) {
-	
-				case 1:
-					System.out.println("Please enter your score:");
-					int numOfScore=in.nextInt();
-					
-					City.prize(numOfScore);///check if there is any error
-					break;
-					
-				case 2:
-			System.out.println("Enter the name of city you want to check:");
-			String nameOfCity=in.next();
-			City.SearchNC(nameOfCity);
-			break;
-			
-				case 3:
-					System.out.println("Enter the name of the Restaurant:");
-					String nameOfRestaurant=in.next();
-					R1.SearchRestaurant(nameOfRestaurant);
-					break;
-					
-				case 4:
-					System.out.println("Enter the name of the Restaurant that you recommend to remove:");
-					System.out.println("shiro sushi ,burgerizzer,Shawarmer ,ALbaik ,Maestro Pizza");
-					String Rname=in.next();
-					//Rname =remove name of the Restaurant.
-					R1.removeRestaurant(Rname);
-					break;
-					
-				case 5:
-				if(CsHotel.isFull())
-					System.out.println("Sorry you can't book in this CS Hotel");
-				else {
-					
-				System.out.println("Please enter the date of check in and the check out:");	
-				int date_Chekin=in.nextInt();
-				int date_Checkout=in.nextInt();
-			
-				
-				System.out.println("Enter number of the room that you want to check in:");
-				int numOfRoom=in.nextInt();
-				
-				if(CsHotel.AddRoom(cus1, numOfRoom)==true) {
-					System.out.println("Congrats you have booked a room");
-					System.out.println("the total coast=\n"+CsHotel.calculatePrice(date_Checkout, date_Chekin));
-				}
-				else 
-					System.out.println("We are sorry there is no room available");
-				
-				}//else
-				System.out.println("your info");
-				System.out.println("==================");
-			
-				System.out.println(cus1.toString());
-				break;
-			}//switch cus number
-				
-			}while(number=='x');
-			break;
 	}//first switch 
 	
 }while(num!=-1);//do while the first
 	
+   for(int i=0;i<counterOfCustemer;i++)
+   System.out.println(cList[i]);
 }//main
 }//class
-
-
-/*if(answer.equalsIgnoreCase("yes")) {
-
-System.out.println("Enter your score :");
-int numScore=in.nextInt();
-////
-City.prize(numScore);}
-else
-	System.out.println("see you next time");
-////*/

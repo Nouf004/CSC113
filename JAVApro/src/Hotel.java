b46bc57d7197a9368b97495c2854611cc49db743
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-03-13 23:24:13 +0300

    Update Hotel.java

--- JAVApro/src/Hotel.java
+++ JAVApro/src/Hotel.java
@@ -1,49 +1,48 @@
 
 public class Hotel extends Carnival { 
 
-public int numOfRooms;
+   public int CounterOfRoooms;
 
- public Hotel(){
- Clist=new Customer[300];
- numOfRooms=0;
- }
+   public Hotel(){
+      Clist=new Customer[300];
+      CounterOfRoooms=0;
+   }
 
- public boolean isFull(){
- if(numOfRooms==Clist.length)
-  return true;
-  return false;
- }
+   public boolean isFull(){
+      if(CounterOfRoooms==Clist.length)
+         return true;
+      return false;
+   }
+
+   public boolean AddRoom(Customer c) {
+      if(searchRoom()){
+         Clist[CounterOfRoooms++]= c;
+         return true;
+      }
+      return false;
+   } 
+
+   public boolean searchRoom(){
+      if(CounterOfRoooms<Clist.length)
+         return true;
+      return false;
+   }
  
- public boolean AddRoom(Customer c,int num) {
- if(searchRoom(num)!=-1){
- Clist[num-1]= c;
-  numOfRooms++;
-  return true;
- }
-  return false;
- } 
-
- public int searchRoom(int num){
-  if( Clist[num-1]==null)
-   return num;
-   return -1;
- }
- 
-public double calculatePrice(int check_outDay,int check_inDay){
-  double total;
-  total=(check_outDay-check_inDay)*300;
-  return total;
-}
+   public double calculatePrice(int check_outDay,int check_inDay){
+      double total;
+      total=(check_outDay-check_inDay)*300;
+      return total;
+   }
 
 
- public void removeRoom(int Rnum,int check_outDay,int check_inDay){
-  int count=0;
-  int duration= check_outDay-check_inDay;
-  for(int i=0; i<duration; i++)
-   count++;
-  if(count>duration) 
-  Clist[Rnum-1]=null;
-  numOfRooms--;
- }
+   public boolean removeRoom(int check_outDay,int check_inDay){
+      int duration= check_outDay-check_inDay;
+      if(duration>3){
+         Clist[CounterOfRoooms--]=null;
+         return true;}
+      return false;}
+      
+   
+  
   
 }//class

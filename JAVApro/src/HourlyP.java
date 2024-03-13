e82a636b9566d1e5a82a1bbd9ca76a7ab6429470
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-03-13 23:22:29 +0300

    Update HourlyP.java

--- JAVApro/src/HourlyP.java
+++ JAVApro/src/HourlyP.java
@@ -1,20 +1,24 @@
 //subclass
 public class HourlyP extends Employee  {
-	private double hour;
-	public HourlyP(String id,int A,double h) {
-		super(id,A);
-		hour=h;
-	}//counstcuter
+   private double hour;
+   public HourlyP(String id,int A,double h) {
+      super(id,A);
+      hour=h;
+   }//counstcuter
 	
 	
-public double getPaid(){
-	if(hour>7)
-	return 50*hour;
-	return 30*hour;
+   public double getPaid(){
+      if(hour>7)
+         return 50*hour;
+      return 30*hour;
+   }
+
+   public String toString() {
+      return super.toString() + "\n hour equals=" + hour + "\nThe salary equals=" + getPaid();
+   }
+public void setHour(double hour){
+this.hour=hour;
 }
-
-public String toString() {
-	return super.toString()+"\nhour equals="+hour+",The salary equals="+getPaid();
-}//method toString
-
+   public double getHour(){
+      return hour;}
 }//class

097f1805b08e8e5582c4960588a53e973acdca34
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-03-13 23:21:40 +0300

    Update Employee.java

--- JAVApro/src/Employee.java
+++ JAVApro/src/Employee.java
@@ -1,28 +1,26 @@
 //superClass
 
 public class Employee {
-protected String ID;
-protected int age;
+   protected String ID;
+   protected int age;
 ////method 
 
-public Employee(String id,int A) {
-ID=id;
-age=A;
-}//constructer
+   public Employee(String id,int A) {
+      ID=id;
+      age=A;
+   }//constructer
 
-public double getPaid() {
-return 0.0;
-}
-public int getAge() {
-	return age;}
+   public double getPaid() {
+      return 0.0;
+   }
+   public int getAge() {
+      return age;}
 		
-public String toString() {	
-	return "\n ID="+ID+"\nAge="+age;
-}
-
+   public String getID(){
+      return ID;}
+   
+   public String toString() {	
+      return "\n ID="+ID+"\n Age="+age;
+   }
 
 }//class
-
-
-
-

6273b54c1034befafbfab58960eee57253ba09f1
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-03-13 23:22:02 +0300

    Update DailyP.java

--- JAVApro/src/DailyP.java
+++ JAVApro/src/DailyP.java
@@ -1,18 +1,25 @@
 //subclass that count the salary
 public class DailyP extends Employee {
-private int day;
-public DailyP(String id,int A,int d) {
-	super(id,A);
-	day=d;
-}
-public double getPaid() {
-	if(day>30)
-		return day*100;
-	return day*60;
-}//method 
+   private int day;
 
-public String toString() {
-	return super.toString()+"\n day equals="+day+", The salary is equals= "+getPaid();
-}//method toString
+   public DailyP(String id,int A,int d) {
+      super(id,A);
+      day=d;
+   }
+   public double getPaid() {
+      if(day>30)
+         return day*100;
+      return day*60;
+   }//method 
+
+public void setDay(int day){
+this.day=day;}
+
+   public int getDay(){
+      return day;}
+
+   public String toString() {
+      return super.toString() + "\n day equals=" + day + "\n The salary is equals= " + getPaid();
+   }
+
 }//class
-

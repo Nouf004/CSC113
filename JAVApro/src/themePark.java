90fbebf74eceaa994b10c60b2d4888822c02de08
Nouf004 <160863269+Nouf004@users.noreply.github.com>
2024-03-15 12:28:55 +0300

    Update themePark.java

--- JAVApro/src/themePark.java
+++ JAVApro/src/themePark.java
@@ -1,56 +1,61 @@
 
 public class themePark {
-   public String[]CityList;
+public String[]CityList;
 
-   Customer[]customerlist;
-   public int numOfCustomer;
+Customer[]customerlist;
+public int numOfCustomer;
 
-   public themePark() {
-      CityList=new String[]{"Italy","Japan","France","India","Riyadh"};
-      customerlist=new Customer[300];
-      numOfCustomer=0;
-   }
-
-
-   public int SearchNC(String name) {
-   
-      for(int i=0;i<CityList.length;i++) 
-      
-         if(CityList[i].equalsIgnoreCase(name))
-         
-            return i;
-   
-      return -1;	
-   }//NC=name of city
+public themePark() {
+	CityList=new String[]{"Italy","Japan","France","India","Riyadh"};
+	customerlist=new Customer[300];
+	numOfCustomer=0;
+}
 
 
-   public void prize(int numS) {
-      if(numS>1500)
-         System.out.println("Congratulations you won Car");
-      else
-         if(numS>1000&&numS<1500)
-            System.out.println("Congratulations you won London");
-         else
-            if(numS<1000)
-               System.out.println("Congratulations you won 200K");
+public int SearchNC(String name) {
+	
+	for(int i=0;i<CityList.length;i++) 
+		
+if(CityList[i].equalsIgnoreCase(name))
+	
+return i;
+	
+	return -1;	
+}//NC=name of city
+
+
+public void prize(int numS) {
+	if(numS>1500)
+		System.out.println("Congratulations you won Car");
+	else
+		if(numS>1000&&numS<1500)
+			System.out.println("Congratulations you won London");
+		else
+			if(numS<1000)
+				System.out.println("Congratulations you won 200K");
             else 
-               System.out.println("We are sorry but you didn't win");
+            System.out.println("We are sorry but you didn't win");
             
-   }//numS=number of score
+}//numS=number of score
 
-   public boolean addCustomer(Customer C){
-      if( numOfCustomer < customerlist.length ){
-         customerlist [numOfCustomer++] =new Customer(C.getID(),C.getLevel());
-         return true;}
-      else
-         return false;
-   }//////
+ public boolean addCustomer(Customer C){
+if( numOfCustomer < customerlist.length ){
+	customerlist [numOfCustomer++] =new Customer(C.getID(),C.getLevel());
+return true;}
+  else
+return false;
+ }//////
 
 
-   public String toString(){
-      String str="";
-      str+=numOfCustomer;
-      return str; 
-   }//end of to string
 
+public String toString(){
+String str="";
+for(int i=0;i<numOfCustomer;i++)
+str="\n"+customerlist[i].toString();
+
+return str; 
+}//end of to string
+
+public int printNumberOfCustomer(){
+return numOfCustomer;}
 }
